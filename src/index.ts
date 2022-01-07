6079aff9c17a2891dd41bb1151d79b6be1bbd3cf
dasadeco <dasadeco@yahoo.es>
2022-01-07 20:22:38 +0100

    server express

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
-const clubs = require("../es.1.clubs.json");
-console.log(clubs);
+// const clubs = require("../es.1.clubs.json");
+// console.log(clubs);
+import createServer  from "./server";
+
+
+createServer().listen(3000, () =>{
+    console.log("Ya estamos escuchando");   
+})
