31cf09c8215a62ebd01a173f7e88938d416e5999
Daniel Sáenz de Cosca Lacalle <dasadeco@yahoo.es>
2025-04-18 18:34:17 +0200

    Cambios de prueba

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
-// const clubs = require("../es.1.clubs.json");
-// console.log(clubs);
 import createServer  from "./server";
 
 
 createServer().listen(3000, () =>{
-    console.log("Ya estamos escuchando");   
+    console.log("Seguimos escuchando");   
 })
